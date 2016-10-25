ed5e66d25cd1a9762d183aff8167b9ee6348c71d
Shubin, Andrew <andrewbshubin@gmail.com>
2016-10-24 21:21:10 -0700

    Finished in-class #2

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -16,25 +16,20 @@ public class HelloController {
 
     private int count = 0;
 
-    @RequestMapping("/greeting")
-    public Greeting greeting() {
-        return new Greeting(count++, "World");
-    }
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public Greeting greeting() throws IOException {
+        // ObjectMapper provides functionality for reading and writing JSON
+        ObjectMapper mapper = new ObjectMapper();
 
-    @RequestMapping("/greeting2")
-    public String greeting2(@RequestParam(value="name", defaultValue = "World") String name) {
-        return "Hello " + name;
-    }
-
-    @RequestMapping(value="/getHighestGreeting", method= RequestMethod.POST)
-    public Greeting getHighestGreeting(@RequestBody List<Greeting> list) {
-        Greeting highestGreeting = new Greeting(0, "");
-        for (Greeting greet : list) {
-            if (greet.getId() >= highestGreeting.getId()) {
-                highestGreeting = greet;
-            }
+        if (FileUtils.readFileToString(new File("./message.txt")).equals("")) {
+            return new Greeting(count, "File Empty");
         }
-        return highestGreeting;
+        String message = FileUtils.readFileToString(new File("./message.txt"));
+
+        // Deserialize JSON to greeter object
+        Greeting greeting = mapper.readValue(message, Greeting.class);
+
+        return greeting;
     }
 
     @RequestMapping(value = "/updateGreeting", method = RequestMethod.PUT)
@@ -55,5 +50,48 @@ public class HelloController {
 
         return greeting;
     }
+
+    @RequestMapping(value = "/deleteGreeting", method = RequestMethod.DELETE)
+    public void deleteGreeting(@RequestBody int id) throws IOException {
+        // ObjectMapper provides functionality for reading and writing JSON
+        ObjectMapper mapper = new ObjectMapper();
+        String message = FileUtils.readFileToString(new File("./message.txt"));
+
+        // Deserialize JSON to greeter object
+        Greeting greeting = mapper.readValue(message, Greeting.class);
+        if (greeting.getId() == id) {
+            FileUtils.writeStringToFile(new File("./message.txt"), "");
+        }
+    }
+
+    @RequestMapping(value = "/createGreeting", method = RequestMethod.POST)
+    public Greeting createGreeting(@RequestBody String name) throws IOException {
+        // ObjectMapper provides functionality for reading and writing JSON
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Create message
+        Greeting greeting = new Greeting(++count, name);
+
+        // Serialize greeting object to JSON
+        mapper.writeValue(new File("./message.txt"), greeting);
+
+        return greeting;
+    }
+
+//    @RequestMapping("/greeting2")
+//    public String greeting2(@RequestParam(value="name", defaultValue = "World") String name) {
+//        return "Hello " + name;
+//    }
+//
+//    @RequestMapping(value="/getHighestGreeting", method= RequestMethod.POST)
+//    public Greeting getHighestGreeting(@RequestBody List<Greeting> list) {
+//        Greeting highestGreeting = new Greeting(0, "");
+//        for (Greeting greet : list) {
+//            if (greet.getId() >= highestGreeting.getId()) {
+//                highestGreeting = greet;
+//            }
+//        }
+//        return highestGreeting;
+//    }
 
 }
